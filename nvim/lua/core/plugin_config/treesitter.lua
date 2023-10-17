edd261f4c5dd0814cf309b62fef2869fa6d9a2e5
JulOuellet <julien.ouellet@live.ca>
2023-10-16 20:47:14 -0400

    add more lang syntax highligts

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "c", "lua", "rust", "java", "python", "cpp", "vim" },
+    ensure_installed = { "c", "lua", "rust", "java", "python", "cpp", "vim", "latex", "go", "yaml", "sql", "dockerfile", "bash" },
 
     -- Install parsers synchronously (only applied to 'ensure_installed')
     sync_install = false,
