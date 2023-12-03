b34d8124c0f140e2ddd138b3a15bfdb6e181ba78
JulOuellet <julien.ouellet@live.ca>
2023-12-03 16:08:19 -0500

    update nvim-tree config

--- nvim/lua/core/plugin_config/nvim-tree.lua
+++ nvim/lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,11 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+    git = {
+	enable = true,
+	ignore = false, -- set to false to see files included in gitignore in tree
+    },
+}
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
