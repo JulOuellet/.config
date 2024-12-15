c3b9999414093b321a8f69d2144565b661d66fbf
JulOuellet <julien.ouellet@live.ca>
2024-12-14 23:43:07 -0500

    update telescope version

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -21,7 +21,7 @@ return require('packer').startup(function(use)
     use 'nvim-treesitter/nvim-treesitter'
     use {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.1',
+	tag = '0.1.8',
 	requires = { {'nvim-lua/plenary.nvim'} }
     }
     use {
