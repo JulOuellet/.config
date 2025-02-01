fc60646c82c1ccaa1816fae3a398b87aac1b2deb
JulOuellet <julien.ouellet@live.ca>
2025-02-01 10:28:53 -0500

    add vim as global varriable

--- nvim/lua/core/plugin_config/lsp-config.lua
+++ nvim/lua/core/plugin_config/lsp-config.lua
@@ -23,6 +23,13 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").lua_ls.setup {
+    settings = {
+	Lua = {
+	    diagnostics = {
+		globals = {"vim"}
+	    }
+	}
+    },
     on_attach,
     capabilities = capabilities
 }
