6c3f735f6bc7d827c495b188363f78f8607135e8
JulOuellet <julien.ouellet@live.ca>
2025-02-02 16:03:30 -0500

    add options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,5 +1,36 @@
-vim.o.number = true      -- Show line numbers
-vim.opt.showcmd = true   -- Show command in bottom bar
-vim.opt.autowrite = true -- Automatically write before running commands
-vim.opt.autoread = true  -- Automatically read when file is changed from outside
-vim.opt.shiftwidth = 4   -- Size of an indent
+local opt = vim.opt
+
+opt.number = true      -- Show line numbers
+opt.showcmd = true   -- Show command in bottom bar
+opt.autowrite = true -- Automatically write before running commands
+opt.autoread = true  -- Automatically read when file is changed from outside
+
+-- Indentation
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = true
+vim.bo.softtabstop = 4
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Line
+opt.wrap = false
+opt.smartcase = true
+opt.cursorline = true
+
+-- Appearance
+opt.termguicolors = true
+opt.signcolumn = 'yes'
+vim.diagnostic.config = {
+    float = { border = 'rounded' },
+}
+
+-- Backspace
+opt.backspace = 'indent,eol,start'
+
+-- Clipboard
+opt.clipboard:append('unnamedplus')
+
